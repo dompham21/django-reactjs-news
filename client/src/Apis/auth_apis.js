0011e893f6e38d0f09b7e3141e6fe393895e4b96
daitech20 <daindp.tech@gmail.com>
2021-09-28 10:19:35 +0700

    update api

--- client/src/Apis/auth_apis.js
+++ client/src/Apis/auth_apis.js
@@ -3,8 +3,8 @@ import axiosService from "../Helper/axiosService";
 
 export const postLoginApi = (dataToSubmit) => {
     const { email, password } = dataToSubmit
-    return axiosService.post(`/api/login`,{
-       email: "",
-       password: ""
+    return axiosService.post(`/api/token/`,{
+       username: email,
+       password: password
     });
 }
